ba96f19cefee1bab488bb7a5081077c78d2ffc21
uo269412 <uo269412@uniovi.es>
2021-04-20 21:53:46 +0200

    SDI-2021-606-SW-11.1-Servicio SOAP

---
+++ sdi2021-606-lab-soap-sw/src/main/java/com/uniovi/Sdi2021606LabSoapSwApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2021606LabSoapSwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi2021606LabSoapSwApplication.class, args);
+	}
+
+}
